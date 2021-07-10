629118234fe1a34902193e939b02f8fe8d815b5f
SONAL <sonali23jshetty@gmail.com>
2021-07-10 20:13:36 +0530

    fetching data and styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./Components/Card";
 import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+  console.log(pokemonData);
+
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(
         "https://dev-util.edyst.com/challenge/pokemons/random"
       );
-      console.log(request.data);
+      setPokemonData(request.data);
 
       return request;
     }
+
     fetchData();
   }, []);
 
   return (
     <div className="app">
-      <div className="card__holder">
-        <Card />
-        <Card />
-        <Card />
-      </div>
+      {pokemonData.length > 0 && (
+        <div className="card__holder">
+          <Card
+            backgroundColor={pokemonData[0].cardColors.bg}
+            imgBackgroundColor={pokemonData[0].cardColors.imgbg}
+            nextPokemon={pokemonData[1].name}
+            title={pokemonData[0].name}
+            about={pokemonData[0].description}
+            image={pokemonData[0].sprite}
+          />
+          <Card
+            backgroundColor={pokemonData[1].cardColors.bg}
+            imgBackgroundColor={pokemonData[1].cardColors.imgbg}
+            nextPokemon={pokemonData[2].name}
+            title={pokemonData[1].name}
+            about={pokemonData[1].description}
+            image={pokemonData[1].sprite}
+          />
+          <Card
+            backgroundColor={pokemonData[2].cardColors.bg}
+            imgBackgroundColor={pokemonData[2].cardColors.imgbg}
+            nextPokemon={pokemonData[0].name}
+            title={pokemonData[2].name}
+            about={pokemonData[2].description}
+            image={pokemonData[2].sprite}
+          />
+        </div>
+      )}
     </div>
   );
 }
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,13 +2,30 @@ import React from "react";
 
 import "./Card.css";
 
-function Card() {
+function Card({
+  nextPokemon,
+  title,
+  about,
+  backgroundColor,
+  imgBackgroundColor,
+  image,
+}) {
   return (
-    <div className="card">
-      <div className="topleft"></div>
+    <div className="card" style={{ backgroundColor: `${backgroundColor}` }}>
+      <div className="topleft">
+        <h1>{nextPokemon}</h1>
+      </div>
       <div className="top right"></div>
-      <h1>title</h1>
-      <p>about</p>
+
+      <h1>{title}</h1>
+      <p>{about}</p>
+
+      <div
+        className="pokemon__image"
+        style={{ backgroundColor: `${imgBackgroundColor}` }}
+      >
+        <img src={image} alt="pokemon" />
+      </div>
     </div>
   );
 }
